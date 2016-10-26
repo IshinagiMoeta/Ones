3a59a82a94f3bb965babfcec838f5308a9baab0d
yinyib <863224759@qq.com>
2016-10-26 09:43:29 +0800

    second

---
+++ app/src/main/java/com/bystanders/moeta/ones/utils/YcUtils.java
@@ -0,0 +1,12 @@
+package com.bystanders.moeta.ones.utils;
+
+import android.util.Log;
+
+/**
+ * Created by Administrator on 2016/10/26.
+ */
+public class YcUtils {
+    public static void show(){
+        Log.e("===","test");
+    }
+}
